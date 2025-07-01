6ba3511e27911ff129e9ac4305ab032c287a93ef
Alber-Tadrous <albertadrous@gmail.com>
2025-07-01 11:16:26 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/types/database';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://hxrybzrfjrotgzlarxzf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4cnlienJmanJvdGd6bGFyeHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NjgwNDMsImV4cCI6MjA2NjU0NDA0M30.Ni_qP5hpz89QUnZ9wDlePocL2PPIC2e0vGcdfUl33Dg'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
